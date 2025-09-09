0fbeeee1e6f92cf9d48189573abbfb55e2e0229d
HyangMok Baek <57baek@gmail.com>
2025-09-09 18:07:57 -0400

    MLP added

--- autograd/code/neruon.py
+++
@@ -1,5 +0,0 @@
-class Neuron:
-    
-    def __init__(self, n_input):
-        self.weight = 
-        self.bias = 
--- autograd/code/network.py
+++ autograd/code/network.py
@@ -1,104 +1,45 @@
-import math
+import random
+
+from autograd.code.value import Value
+
+random.seed(57)
 
 
-class Node:
-    def __init__(self, data, _children=(), _op="", label=""):
-        self.data = data
-        self.grad = 0.0
-        self._backward = lambda: None
-        self._prev = set(_children)
-        self._op = _op
-        self.label = label
+class Neuron:
+    def __init__(self, nin):
+        self.w = [Value(random.uniform(-1, 1)) for _ in range(nin)]
+        self.b = Value(random.uniform(-1, 1))
 
-    def __repr__(self):
-        return f"Node(data={self.data})"
-
-    def __add__(self, other):
-        other = other if isinstance(other, Node) else Node(other)
-
-        out = Node(self.data + other.data, (self, other), "+")
-
-        def _backward():
-            self.grad += out.grad * 1.0
-            other.grad += out.grad * 1.0
-
-        out._backward = _backward
-
+    def __call__(self, x):
+        act = sum((wi * xi for wi, xi in zip(self.w, x)), self.b)
+        out = act.tanh()
         return out
 
-    def __mul__(self, other):
-        other = other if isinstance(other, Node) else Node(other)
-        out = Node(self.data * other.data, (self, other), "*")
+    def parameters(self):
+        return self.w + [self.b]
 
-        def _backward():
-            self.grad += out.grad * other.data
-            other.grad += out.grad * self.data
 
-        out._backward = _backward
+class Layer:
+    def __init__(self, nin, nout):
+        self.neurons = [Neuron(nin) for _ in range(nout)]
 
-        return out
+    def __call__(self, x):
+        outs = [neuron(x) for neuron in self.neurons]
+        return outs
 
-    def __pow__(self, other):
-        assert isinstance(other, (int, float)), "only supporting int/float powers"
-        out = Node(self.data**other, (self,), f"**{other}")
+    def parameters(self):
+        return [p for neuron in self.neurons for p in neuron.parameters()]
 
-        def _backward():
-            self.grad += out.grad * (other * self.data ** (other - 1))
 
-        out._backward = _backward
+class MLP:
+    def __init__(self, nin, nouts):
+        sz = [nin] + nouts
+        self.layers = [Layer(sz[i], sz[i + 1]) for i in range(len(nouts))]
 
-        return out
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x[0] if len(x) == 1 else x
 
-    def exp(self):
-        x = self.data
-        out = Node(math.exp(x), (self,), "exp")
-
-        def _backward():
-            self.grad += out.grad * out.data
-
-        out._backward = _backward
-
-        return out
-
-    def tanh(self):
-        x = self.data
-        t = (math.exp(2 * x) - 1) / (math.exp(2 * x) + 1)
-
-        out = Node(t, (self,), "tanh")
-
-        def _backward():
-            self.grad += out.grad * (1 - t**2)
-
-        out._backward = _backward
-
-        return out
-
-    def __rmul__(self, other):
-        return self * other
-
-    def __truediv__(self, other):
-        return self * other**-1
-
-    def __neg__(self):
-        return self * -1
-
-    def __sub__(self, other):
-        return self + (-1 * other)
-
-    def backward(self):
-        topo = []
-        visited = set()
-
-        def build_topo(v):
-            if v not in visited:
-                visited.add(v)
-                for child in v._prev:
-                    build_topo(child)
-                topo.append(v)
-
-        build_topo(self)
-
-        self.grad = 1.0
-
-        for node in reversed(topo):
-            node._backward()
+    def parameters(self):
+        return [p for layer in self.layers for p in layer.parameters()]
