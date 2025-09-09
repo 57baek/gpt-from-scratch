2e34dfee1312999a5fefd81a70bf8f5ebdef1e02
HyangMok Baek <57baek@gmail.com>
2025-09-09 14:13:25 -0400

    add Neuron

---
+++ autograd/code/neruon.py
@@ -0,0 +1,5 @@
+class Neuron:
+    
+    def __init__(self, n_input):
+        self.weight = 
+        self.bias = 
---
+++ autograd/code/network.py
@@ -0,0 +1,104 @@
+import math
+
+
+class Node:
+    def __init__(self, data, _children=(), _op="", label=""):
+        self.data = data
+        self.grad = 0.0
+        self._backward = lambda: None
+        self._prev = set(_children)
+        self._op = _op
+        self.label = label
+
+    def __repr__(self):
+        return f"Node(data={self.data})"
+
+    def __add__(self, other):
+        other = other if isinstance(other, Node) else Node(other)
+
+        out = Node(self.data + other.data, (self, other), "+")
+
+        def _backward():
+            self.grad += out.grad * 1.0
+            other.grad += out.grad * 1.0
+
+        out._backward = _backward
+
+        return out
+
+    def __mul__(self, other):
+        other = other if isinstance(other, Node) else Node(other)
+        out = Node(self.data * other.data, (self, other), "*")
+
+        def _backward():
+            self.grad += out.grad * other.data
+            other.grad += out.grad * self.data
+
+        out._backward = _backward
+
+        return out
+
+    def __pow__(self, other):
+        assert isinstance(other, (int, float)), "only supporting int/float powers"
+        out = Node(self.data**other, (self,), f"**{other}")
+
+        def _backward():
+            self.grad += out.grad * (other * self.data ** (other - 1))
+
+        out._backward = _backward
+
+        return out
+
+    def exp(self):
+        x = self.data
+        out = Node(math.exp(x), (self,), "exp")
+
+        def _backward():
+            self.grad += out.grad * out.data
+
+        out._backward = _backward
+
+        return out
+
+    def tanh(self):
+        x = self.data
+        t = (math.exp(2 * x) - 1) / (math.exp(2 * x) + 1)
+
+        out = Node(t, (self,), "tanh")
+
+        def _backward():
+            self.grad += out.grad * (1 - t**2)
+
+        out._backward = _backward
+
+        return out
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __truediv__(self, other):
+        return self * other**-1
+
+    def __neg__(self):
+        return self * -1
+
+    def __sub__(self, other):
+        return self + (-1 * other)
+
+    def backward(self):
+        topo = []
+        visited = set()
+
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+
+        build_topo(self)
+
+        self.grad = 1.0
+
+        for node in reversed(topo):
+            node._backward()
